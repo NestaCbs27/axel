0d1189319f753e8dddba2a65ec9f24443f530d64
NestaCbs27 <97601193+NestaCbs27@users.noreply.github.com>
2023-02-22 15:59:42 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 // Team slider
 new Splide( '#splide1', {
     type: "loop",
-    perPage: 4,
+    perPage: 6,
     autoScroll: {
-        speed: 1.5,
+        speed: 2,
     },
     arrows: false,
     pagination: false,
     autoWidth: true,
 } ).mount(window.splide.Extensions);
+
+function displayCGU(){
+    if($('.cgu').hasClass('hidden')){
+      $('.cgu').removeClass('hidden')
+      document.getElementById("cgu").scrollIntoView();
+    }else{
+      $('.cgu').addClass('hidden')
+    }
+    
+  }
